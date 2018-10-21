21944a1997499356e5ee4ba59533826f54878f2c
Matthew Lean <matt@mattlean.com>
2018-10-20 20:13:00 -0700

    Add toObject() to reply model

--- src/models/reply.js
+++ src/models/reply.js
@@ -1,18 +1,28 @@
 // @flow
-
 const mongoose = require('mongoose')
 
-const schema = new mongoose.Schema({
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    required: true
-  },
+const schemaType = 'Reply'
+const schema = new mongoose.Schema(
+  {
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      required: true
+    },
 
-  comment: {
-    type: String,
-    required: true
+    comment: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    toObject: {
+      transform: (doc, ret) => {
+        ret.type = schemaType
+        return ret
+      }
+    }
   }
-})
+)
 
 module.exports = schema
