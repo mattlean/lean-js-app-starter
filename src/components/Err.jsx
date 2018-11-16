65ee32560f885931648e92662fc818129afd4ef1
Matthew Lean <matt@mattlean.com>
2018-11-15 20:19:28 -0800

    Setup enzyme-to-json as snapshot serializer, setup tests for components

--- src/components/Err.jsx
+++ src/components/Err.jsx
@@ -6,14 +6,22 @@ import HTTPErr from '../util/HTTPErr'
 
 const Err = ({ data }: { data: HTTPErr }) => {
   let status
-  if(data.res && data.res.status) {
-    status = <h2 className="center">{data.res.status}</h2>
+  let message
+
+  if(data) {
+    if(data.res && data.res.status) {
+      status = <h2 className="center">{data.res.status}</h2>
+    }
+
+    if(data.message) {
+      message = <p className="center"><i>{data.message}</i></p>
+    }
   }
 
   return (
     <div id="not-found">
       {status}
-      <p className="center"><i>{data.message}</i></p>
+      {message}
       <span className="center">[<Link to="/">Back to Home</Link>]</span>
     </div>
   )
---
+++ src/components/__tests__/Threads.test.jsx
@@ -0,0 +1,29 @@
+import Adapter from 'enzyme-adapter-react-16'
+import Enzyme, { shallow } from 'enzyme'
+import React from 'react'
+
+import Threads from '../Threads'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('Threads', () => {
+  it('renders properly', () => {
+    const data = [
+      {
+        _id: '5bee2f90c43e83433b5e5d25',
+        createdAt: '2018-11-16T02:46:40.311Z',
+        comment: 'All your base are belong to us.',
+        type: 'Threads'
+      },
+      {
+        _id: '5bee445ec43e83433b5e5d36',
+        comment: 'I\'m the thread comment.',
+        createdAt: '2018-11-16T04:15:26.796Z',
+        replies: [],
+        type: 'Thread'
+      }
+    ]
+    const component = shallow(<Threads threads={data} />)
+    expect(component).toMatchSnapshot()
+  })
+})
