a570d3a78500b2fbed927437c28b577ca8d0ab47
Matthew Lean <matt@mattlean.com>
2018-12-01 03:11:33 -0800

    Refactor to use new util/store.js

--- src/util/store.js
+++ src/util/store.js
@@ -1,21 +1,17 @@
-// @flow
-import thunk from 'redux-thunk'
 import { applyMiddleware, compose, createStore } from 'redux'
-import { createLogger } from 'redux-logger'
 
-import todos from '../reducers'
+export const setupStore = (reducer, preloadedState, middlewares = [], devMiddlewares) => {
+  if(!middlewares) middlewares = []
 
-export const setupStore = () => {
-  const middlewares = [thunk]
-
-  if(process.env.NODE_ENV === 'development') {
-    middlewares.push(createLogger())
+  if(process.env.NODE_ENV === 'development' && Array.isArray(devMiddlewares)) {
+    middlewares = [...middlewares, ...devMiddlewares]
   }
 
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
-  return createStore(
-    todos,
-    composeEnhancers(applyMiddleware(...middlewares))
-  )
+  const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  if(preloadedState !== null && preloadedState !== undefined) {
+    return createStore(reducer, preloadedState, composeEnhancers(applyMiddleware(...middlewares)))
+  }
+  return createStore(reducer, composeEnhancers(applyMiddleware(...middlewares)))
 }
+
+export default setupStore
