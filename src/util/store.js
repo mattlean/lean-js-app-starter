4d064a81ae318b5101439eee85aa6b936754640f
Matthew Lean <matt@mattlean.com>
2018-12-01 03:20:03 -0800

    Remove redux-logger, use new util/store.js

--- src/util/store.js
+++ src/util/store.js
@@ -1,21 +1,17 @@
-import thunk from 'redux-thunk'
 import { applyMiddleware, compose, createStore } from 'redux'
-import { createLogger } from 'redux-logger'
 
-import rootReducer from '../reducers'
+export const setupStore = (reducer, preloadedState, middlewares = [], devMiddlewares) => {
+  if(!middlewares) middlewares = []
 
-export const setupStore = preloadedState => {
-  const middlewares = [thunk]
-
-  if(process.env.NODE_ENV === 'development') {
-    middlewares.push(createLogger())
+  if(process.env.NODE_ENV === 'development' && Array.isArray(devMiddlewares)) {
+    middlewares = [...middlewares, ...devMiddlewares]
   }
 
   const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-  if(preloadedState) {
-    return createStore(rootReducer, preloadedState, composeEnhancers(applyMiddleware(...middlewares)))
+  if(preloadedState !== null && preloadedState !== undefined) {
+    return createStore(reducer, preloadedState, composeEnhancers(applyMiddleware(...middlewares)))
   }
-  return createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)))
+  return createStore(reducer, composeEnhancers(applyMiddleware(...middlewares)))
 }
 
 export default setupStore
