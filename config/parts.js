de01afb9f6e4f089f9759b778ea1a0b07e1d0396
Matthew Lean <matt@mattlean.com>
2019-01-11 04:59:54 -0800

    Remove unnecessary parts dependencies & code

--- config/parts.js
+++ config/parts.js
@@ -1,56 +1,10 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-const cssnano = require('cssnano')
 const FlowWebpackPlugin = require('flow-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const nodeExternals = require('webpack-node-externals')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const PurifyCSSPlugin = require('purifycss-webpack')
 const UglifyWebpackPlugin = require('uglifyjs-webpack-plugin')
 const webpack = require('webpack')
 
 const PATHS = require('../PATHS')
-
-// Load files
-const loadFiles = ({ exclude, include, options, test, type } = {}) => {
-  if(type === 'url' || type === undefined) {
-    return {
-      module: {
-        rules: [
-          {
-            use: {
-              loader: 'url-loader',
-              options
-            },
-            exclude,
-            include,
-            test
-          }
-        ]
-      }
-    }
-  } else if(type === 'file') {
-    return {
-      module: {
-        rules: [
-          {
-            use: {
-              loader: 'file-loader',
-              options
-            },
-            test
-          }
-        ]
-      }
-    }
-  }
-}
-
-// Autoprefix CSS
-exports.autoprefix = () => ({
-  loader: 'postcss-loader',
-  options: { plugins: () => [require('autoprefixer')()] }
-})
 
 // Run Flow type checking
 exports.checkTypes = () => ({
@@ -65,50 +19,8 @@ exports.cleanPaths = paths => ({
   )]
 })
 
-// Extract styles into its own CSS file
-exports.extractStyles = ({ exclude, filename, include, use = [] }) => {
-  const plugin = new MiniCssExtractPlugin({ filename })
-
-  return {
-    module: {
-      rules: [
-        {
-          use: [MiniCssExtractPlugin.loader].concat(use),
-          exclude,
-          include,
-          test: /\.(css|scss)$/
-        }
-      ]
-    },
-    plugins: [plugin]
-  }
-}
-
 // Create source maps
 exports.genSourceMaps = ({ type }) => ({ devtool: type })
-
-// Load fonts
-exports.loadFonts = ({ exclude, include, options, type } = {}) => loadFiles({
-  exclude,
-  include,
-  options,
-  test: /\.(eot|ttf|woff|woff2)$/,
-  type
-})
-
-// Load HTML
-exports.loadHTML = options => ({
-  plugins: [ new HtmlWebpackPlugin(options) ]
-})
-
-// Load images
-exports.loadImgs = ({ exclude, include, options, type } = {}) => loadFiles({
-  exclude,
-  include,
-  options,
-  test: /\.(gif|jpe?g|png)$/i,
-  type
-})
 
 // Load JavaScript through Babel
 exports.loadJS = ({ exclude, include } = {}) => ({
@@ -124,47 +36,11 @@ exports.loadJS = ({ exclude, include } = {}) => ({
   }
 })
 
-// Setup styles
-exports.loadStyles = ({ exclude, include } = {}) => ({
-  module: {
-    rules: [{
-      use: [
-        'style-loader',
-        {
-          loader: 'css-loader',
-          options: { sourceMap: true }
-        },
-        {
-          loader: 'sass-loader',
-          options: { sourceMap: true }
-        }
-      ],
-      exclude,
-      include,
-      test: /\.(css|scss)$/
-    }]
-  }
-})
-
-// Minify CSS
-exports.minCSS = ({ options } = {}) => ({
-  plugins: [new OptimizeCSSAssetsPlugin({
-    canPrint: false,
-    cssProcessor: cssnano,
-    cssProcessOptions: options
-  })]
-})
-
 // Minify JavaScript
 exports.minJS = () => ({
   optimization: {
     minimizer: [new UglifyWebpackPlugin( { sourceMap: true} )]
   }
-})
-
-// Remove unused CSS
-exports.purifyCSS = ({ paths }) => ({
-  plugins: [new PurifyCSSPlugin({ paths })]
 })
 
 // Ignore node_modules dependencies in bundling
@@ -177,18 +53,3 @@ exports.setFreeVariable = (key, val) => {
 
   return { plugins: [new webpack.DefinePlugin(env)] }
 }
-
-// Setup webpack-dev-server
-exports.setupDevServer = ({ host, historyApiFallback, hot, open, port, proxy } = {}) => ({
-  devServer: {
-    host,
-    port,
-    historyApiFallback,
-    hot,
-    open,
-    proxy,
-    stats: 'errors-only',
-    overlay: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
-})
