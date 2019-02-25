452e5b361d1bb1e4ab9e71ba7e21ae554c697b51
Matthew Lean <matt@mattlean.com>
2019-02-25 13:22:04 -0800

    Remove unnecessary expect.assertions for for API test

--- src/util/__tests__/api.test.js
+++ src/util/__tests__/api.test.js
@@ -5,16 +5,12 @@ describe('API', () => {
   let thread
 
   test('getThreads() lists all threads', () => {
-    expect.assertions(1)
-
     return getThreads().then(res => {
       expect(Array.isArray(res)).toBe(true)
     })
   })
 
   test('postThread({DATA}) creates a thread', () => {
-    expect.assertions(3)
-
     return postThread(newThread).then(res => {
       thread = res
 
@@ -25,16 +21,12 @@ describe('API', () => {
   })
 
   test('getThreads({ID}) reads specific thread', () => {
-    expect.assertions(1)
-
     return getThreads(thread._id).then(res => {
       expect(res._id).toBe(thread._id)
     })
   })
 
   test('postReply() creates a reply', () => {
-    expect.assertions(1)
-
     const replyData = { comment: threads[0].replies[0].comment }
 
     return postReply(thread._id, replyData).then(res => {
