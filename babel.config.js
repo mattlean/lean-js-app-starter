f5b30d236b199f4d3e1ec69ff153b42a3e4cbd24
Matthew Lean <matt@mattlean.com>
2022-09-08 01:30:46 -0700

    move stuff

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-/**
- * This config file only affects Jest tests.
- * To configure how Babel handles the application build,
- * configure the webpack config's babel-loader instead.
- */
-module.exports = {
-  presets: ['@babel/preset-env'],
-}
