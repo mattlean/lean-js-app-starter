05d178386a1b53489d36a44492f12cd366d3e5fc
Matthew Lean <matt@mattlean.com>
2023-06-02 06:43:18 -0700

    setup prettier import sort

--- starters/react-frontend/src/HelloWorld.test.js
+++ starters/react-frontend/src/HelloWorld.test.js
@@ -1,5 +1,6 @@
-import HelloWorld from './HelloWorld'
 import renderer from 'react-test-renderer'
+
+import HelloWorld from './HelloWorld'
 
 test('HelloWorld component matches snapshot', () => {
     const tree = renderer.create(<HelloWorld />).toJSON()
--- starters/react-frontend/src/main.js
+++ starters/react-frontend/src/main.js
@@ -1,6 +1,7 @@
-import HelloWorld from './HelloWorld'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import HelloWorld from './HelloWorld'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
