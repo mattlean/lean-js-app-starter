d6fe2003007dfd620af76f33c201b0c0858957aa
Matthew Lean <matt@mattlean.com>
2023-06-19 16:58:58 -0700

    sync clearMocks to true

--- starters/basic-nodejs-ts/jest.config.js
+++ starters/basic-nodejs-ts/jest.config.js
@@ -14,7 +14,7 @@ module.exports = {
     // cacheDirectory: "/private/var/folders/93/6f07b_1j45bddtnfvz2vryjr0000gn/T/jest_dx",
 
     // Automatically clear mock calls, instances, contexts and results before every test
-    // clearMocks: false,
+    clearMocks: true,
 
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
