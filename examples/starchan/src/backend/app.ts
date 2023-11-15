8529055a37aacc90d9f1af1e1bcbfa5825e70d5a
Matthew Lean <matt@mattlean.com>
2023-11-15 01:40:43 -0800

    correct paths used by tests for starchan backend

--- examples/starchan/src/backend/app.ts
+++ examples/starchan/src/backend/app.ts
@@ -3,7 +3,7 @@ import express from 'express'
 import morgan from 'morgan'
 import path from 'path'
 
-import { PATH_BACKEND_SRC, PATH_BUILD } from '../../PATHS'
+import { PATH_BACKEND_SRC, PATH_BUILD, PATH_FRONTEND_BUILD } from '../../PATHS'
 import {
     createNotFoundErrorHandler,
     globalErrorHandler,
@@ -52,8 +52,21 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/', frontendHandler)
 
 // Serve the frontend build directory and the backend public directory as static files
-app.use('/', express.static(BUNDLED_FRONT_BUILD_PATH))
-app.use('/static', express.static(`${BUNDLED_BACK_BUILD_PATH}/public`))
+let frontBuildPath
+if (process.env.NODE_ENV === 'test') {
+    frontBuildPath = PATH_FRONTEND_BUILD
+} else {
+    frontBuildPath = BUNDLED_FRONT_BUILD_PATH
+}
+app.use('/', express.static(frontBuildPath))
+
+let backPublicPath
+if (process.env.NODE_ENV === 'test') {
+    backPublicPath = `${PATH_BACKEND_SRC}/public`
+} else {
+    backPublicPath = `${BUNDLED_BACK_BUILD_PATH}/public`
+}
+app.use('/static', express.static(backPublicPath))
 
 // Mount router on /api paths with handlers that respond with JSON
 app.use('/api', apiHandler)
