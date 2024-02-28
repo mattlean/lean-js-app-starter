835f187fb23e9040da384fb5880dd32220b3fd72
Matthew Lean <matt@mattlean.com>
2024-02-28 02:21:02 -0800

    update mongo init.sh scripts

--- examples/starchan/init.sh
+++ examples/starchan/init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# MongoDB Version
+
 PREFIX="[🚀 init.sh]"
 
 echo "${PREFIX} Beginning the initialization script..."
 
-if [ -d "./node_modules" ]
-then
+if [ -d "./node_modules" ]; then
     echo "${PREFIX} The node_modules directory already exists, so skip package dependency installation." 
 else
     echo "${PREFIX} Installing package dependencies..."
@@ -21,13 +22,18 @@ echo "${PREFIX} Generate the Prisma client..."
 npm run prisma generate
 echo "${PREFIX} Prisma client generation completed!"
 
-if [ -f "./build/backend/server.js" ]
-then
-    echo "${PREFIX} The build already exists, so skip the initial build process."
-else
-    echo "${PREFIX} Starting the build process..."
-    npm run build
+if [ "${NODE_ENV}" == "production" ]; then
+    echo "${PREFIX} Starting the production build process..."
+    npm run build:production
     echo "${PREFIX} Build process completed!"
+else
+    if [ -f "./build/backend/server.js" ]; then
+        echo "${PREFIX} The development build already exists, so skip the initial build process."
+    else
+        echo "${PREFIX} Starting the development build process..."
+        npm run build
+        echo "${PREFIX} Build process completed!"
+    fi
 fi
 
 echo "${PREFIX} Initialization script completed!"
--- starters/express-mongo-ts/init.sh
+++ starters/express-mongo-ts/init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# MongoDB Version
+
 PREFIX="[🚀 init.sh]"
 
 echo "${PREFIX} Beginning the initialization script..."
 
-if [ -d "./node_modules" ]
-then
+if [ -d "./node_modules" ]; then
     echo "${PREFIX} The node_modules directory already exists, so skip package dependency installation." 
 else
     echo "${PREFIX} Installing package dependencies..."
@@ -13,21 +14,26 @@ else
     echo "${PREFIX} Package dependency installation completed!"
 fi
 
-echo "${PREFIX} Starting the database migrations..."
-npm run prisma migrate dev
-echo "${PREFIX} Database migrations completed!"
+echo "${PREFIX} Starting the database updates..."
+npm run prisma db push
+echo "${PREFIX} Database updates completed!"
 
 echo "${PREFIX} Generate the Prisma client..."
 npm run prisma generate
 echo "${PREFIX} Prisma client generation completed!"
 
-if [ -f "./build/server.js" ]
-then
-    echo "${PREFIX} The build already exists, so skip the initial build process."
-else
-    echo "${PREFIX} Starting the build process..."
-    npm run build
+if [ "${NODE_ENV}" == "production" ]; then
+    echo "${PREFIX} Starting the production build process..."
+    npm run build:production
     echo "${PREFIX} Build process completed!"
+else
+    if [ -f "./build/backend/server.js" ]; then
+        echo "${PREFIX} The development build already exists, so skip the initial build process."
+    else
+        echo "${PREFIX} Starting the development build process..."
+        npm run build
+        echo "${PREFIX} Build process completed!"
+    fi
 fi
 
 echo "${PREFIX} Initialization script completed!"
